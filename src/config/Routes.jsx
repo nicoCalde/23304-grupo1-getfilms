c0c159f96d34fd82d4cd0053a240e06ab70dcd39
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-07-04 15:05:58 -0300

    Pages + Components added

---
+++ src/config/Routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Catalog from '../pages/Catalog';
+import Detail from '../pages/Detail';
+
+const Routes = () => {
+  return (
+    <Switch>
+        
+    </Switch>
+  )
+}
+
+export default Routes;
+
