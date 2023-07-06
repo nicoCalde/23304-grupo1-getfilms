f5920be1e12bd3c7e662642e5881c63897aed170
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-07-06 18:13:47 -0300

    catalog + load more created

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useParams } from 'react-router';
 
 import PageHeader from '../components/page-header/PageHeader';
+import MovieGrid from '../components/movie-grid/MovieGrid';
 
 import { category as cate } from '../api/tmdbApi';
 
@@ -15,6 +16,11 @@ const Catalog = () => {
       <PageHeader>
         {category === cate.movie ? 'Péliculas' : 'Series'}
       </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category}/>
+        </div>
+      </div>
     </>
   )
 }
